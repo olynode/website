a0b131b2bbfb261db66b27527c20c3487ae59738
Mark Lybrand <mlybrand@gmail.com>
2012-06-19 07:24:08 -0700

    adding simple existence test for headImage, more to follow..

---
+++ test/headImage_tests.js
@@ -0,0 +1,8 @@
+var headImage = require('../lib/headImage'),
+    should = require('should');
+
+describe('headImage', function() {
+  it('exists', function() {
+    should.exist(headImage);
+  });
+});
