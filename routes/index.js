939a5fcc533849e2ea3e3933e769ae8aee07ff04
Mark Lybrand <mlybrand@gmail.com>
2012-06-18 21:58:38 -0700

    added first stab at random images. needs refactoring

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var meetup = require('../lib/meetupApi');
+var headImage = require('../lib/headImage');
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
   meetup.getMeetings(function(meetings){
-	  res.render('index', { title: 'Welcome to OlyNode', meetings: meetings });
+	  res.render('index', { title: 'Welcome to OlyNode', meetings: meetings, headImage: headImage.getImage() });
   });
 
 };
