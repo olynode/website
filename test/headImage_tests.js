5b9b2c39f598375719f825c6831c96217fdd3274
Mark Lybrand <mlybrand@gmail.com>
2012-06-19 21:41:25 -0700

    finished basic tests for headImage

--- test/headImage_tests.js
+++ test/headImage_tests.js
@@ -5,4 +5,28 @@ describe('headImage', function() {
   it('exists', function() {
     should.exist(headImage);
   });
+
+  describe("getImage", function() {
+    var img;
+
+    beforeEach(function() { 
+      img = headImage.getImage();
+    });
+
+    it("gets an image", function() {
+      img.should.not.be.null;
+    });
+
+    it("has an image path", function() {
+      img.url.should.not.be.empty;
+    });
+
+    it("has an original source url", function() {
+      img.srcUrl.should.not.be.empty;
+    });
+
+    it("has a source author", function() {
+      img.srcAuthor.should.not.be.empty;
+    });
+  });
 });
