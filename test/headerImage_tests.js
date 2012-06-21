b95d079894b2c12922535475ae0855396700ee9f
Bobby Johnson <bobby.johnson@gmail.com>
2012-06-20 21:06:46 -0700

    removed file

--- test/headerImage_tests.js
+++
@@ -1,8 +0,0 @@
-var headerImage = require('../lib/headImage'),
-    should = require('should');
-
-describe('header image', function(){
-	it('should exist', function(){
-		headerImage.should.exist;
-	});
-});
