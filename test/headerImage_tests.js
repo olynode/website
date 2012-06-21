70335ed0b9043617ad85ffbe8a422a7d06ecaa4f
Bobby Johnson <bobby.johnson@gmail.com>
2012-06-20 21:05:46 -0700

    minor organization of includes

---
+++ test/headerImage_tests.js
@@ -0,0 +1,8 @@
+var headerImage = require('../lib/headImage'),
+    should = require('should');
+
+describe('header image', function(){
+	it('should exist', function(){
+		headerImage.should.exist;
+	});
+});
