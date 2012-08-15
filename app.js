e9e1f9436583619259e3916cdb24c334a0768aac
Bobby Johnson <bobby.johnson@gmail.com>
2012-08-14 17:39:49 -0700

    adding twitter metadata

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 
 app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
