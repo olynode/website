41188bb948b74c3c1dfe55715ac8bc056e6eb68c
Bobby Johnson <bobby.johnson@gmail.com>
2012-11-12 19:50:02 -0800

    adding meetup api to worker

--- worker.js
+++ worker.js
@@ -1,1 +1,11 @@
-console.log("worker run");
+var meetup = require('./lib/meetupApi');
+
+meetup.getMeetings(function(meetings){
+	meetings.forEach(function(meeting){
+		//check if meeting.id exist in db
+			//check if meeting.updated date is newer
+				//update existing meeting
+		//else insert meeting
+		console.log(meeting);
+	});
+});
