3a6179ec43a0bd3ddd6d3e6b022e8bf5296a23f2
Bobby Johnson <bobby.johnson@gmail.com>
2012-12-12 18:02:11 -0800

    modified index route to use the synced data in mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-var meetup = require('../lib/meetupApi');
-var headImage = require('../lib/headImage');
+var database = require('../lib/database'),
+	headImage = require('../lib/headImage');
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  meetup.getMeetings(function(meetings){
-	  res.render('index', { 
+  database.getMeetings(function(error, meetings){
+  	  res.render('index', { 
 	  	title: 'Welcome to OlyNode', 
 	  	meetings: meetings, 
 	  	headImage: headImage.getImage() 
