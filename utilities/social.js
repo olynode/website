7be9bb1be653f84330431929c6357b95786262b2
Bobby Johnson <bobby.johnson@gmail.com>
2012-12-13 19:08:04 -0800

    removing cucumber as a dependency

---
+++ utilities/social.js
@@ -0,0 +1,26 @@
+var twitter = require('ntwitter'),
+	database = require('../lib/database');
+
+
+var twit = new twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+twit.updateStatus('Test tweet from ntwitter/' + twitter.VERSION, function(error, data) {
+	if(error) console.log(error);
+	console.log(data);
+});
+
+var tweetMeeting  = function(error, meeting){
+	console.log(meeting[0]);
+	twit.updateStatus(meeting[0].name, function(error, data) {
+		if(error) console.log(error);
+		console.log(data);
+	});
+};
+
+database.findMeetingBy({ time: { $gte: Date.now() } }, tweetMeeting);
+
